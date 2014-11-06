c6c17de55ac7ec828e4d164376075d920c0ef1bd
Nick Herman <nherman@gmail.com>
2014-11-06 17:21:02 -0500

    improced validation

--- poolscore/routes.py
+++ poolscore/routes.py
@@ -1,18 +1,44 @@
+from functools import wraps
 from flask import g, session, render_template, request, redirect, url_for, flash
 from . import app, get_db
 from .database.entities import User
 
+#decorators
+def validateAccess(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not session.get('activeuser'):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated
 
+
+#helpers
+def validate_login(req):
+    if req.form['username'] == "" or req.form['username'] == None:
+        return "Please enter your user name."
+
+    if req.form['password'] == "" or req.form['password'] == None:
+        return "Please enter your password."
+
+    data = get_db().get_password_by_username(req.form['username'])
+
+    if data == None or not data['active']:
+        return "Username doesn't exist"
+
+    if data['password'] != req.form['password']:
+        return "Password is incorrect"
+
+    return 0
+
+#routes
 @app.route('/')
+@validateAccess
 def root():
-    if session.get('activeuser'):
-        user = get_db().get_user_by_name(session.get('activeuser'))
-        print(user.date_created)
+    user = get_db().get_user_by_name(session.get('activeuser'))
+    print(user.date_created)
 
-        return render_template('index.html', test=app.config['DATABASE'])
-
-    #NO Login - redirect user to login
-    return redirect(url_for('login'))
+    return render_template('index.html', test=app.config['DATABASE'])
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -35,23 +61,4 @@ def logout():
     return redirect(url_for('root'))
 
 
-
-#helpers
-
-def validate_login(req):
-    if req.form['username'] == "" or req.form['username'] == None:
-        return "Please enter your user name."
-
-    if req.form['password'] == "" or req.form['password'] == None:
-        return "Please enter your password."
-
-    data = get_db().get_password_by_username(req.form['username'])
-
-    if data == None or not data['active']:
-        return "Username doesn't exist"
-
-    if data['password'] != req.form['password']:
-        return "Password is incorrect"
-
-    return 0
 
