a1de7658d87fe8a4782ff103137c38a24719f361
Nick Herman <nherman@gmail.com>
2015-01-31 13:28:41 -0500

    defined ruleset class. added apa8ball ruleset.  defined game, match and tourney events for apa8ball.

--- poolscore/rules/rulesets.py
+++ poolscore/rules/rulesets.py
@@ -2,13 +2,23 @@ import handicaps
 
 class Ruleset(object):
 
-    def __init__(self, name, handicaper, **kwargs):
+    def __init__(self,
+                 name,
+                 handicaper,
+                 tourney_events = {},
+                 match_events = {},
+                 game_events = {},
+                 **kwargs):
         self.name = name
         self.handicaper = handicaper
+        self.tourney_events = tourney_events
+        self.match_events = match_events
+        self.game_events = game_events
 
         for key in kwargs:
-            if self[key] != None:
-                self[key] = kwargs[key]
+            if key not in self.__dict__:
+                self.__dict__[key] = kwargs[key]
+
 
     def handicap(*args):
         if args.length >= 2:
@@ -16,10 +26,38 @@ class Ruleset(object):
         else:
             raise TypeError
 
+# "static"
+# {"event_name": ( ["Valid","Values"], "Default Value" ) }
+apa8ball_tourney_events = {
+    "coin_toss": (int,None),
+    "player_choice": (int,None)
+}
+
+apa8ball_match_events = {
+    "lag": (int,None),
+    "sweep": (bool,False),
+    "rubber": (bool,False)
+}
+
+apa8ball_game_events = {
+    "breaker":(int,None),
+    "innings": (int,0),
+    "home_coaches": (int,0),
+    "home_safes": (int,0),
+    "away_coaches": (int,0),
+    "away_safes": (int,0),
+    "8 break": (bool,False),
+    "break & run": (bool,False),
+    "early 8": (bool,False),
+    "scratch 8": (bool,False)
+}
 
 RULESETS = {
     "APA8BALL": Ruleset(
-                    "APA8BALL",
-                    handicaps.APA8BALL
+                    name = "APA8BALL",
+                    handicaper = handicaps.APA8BALL,
+                    tourney_events = apa8ball_tourney_events,
+                    match_events = apa8ball_match_events,
+                    game_events = apa8ball_game_events
                 )
 }
