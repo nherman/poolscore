e27f93e4650205383dae4b64c2fdb31e02789784
Nick Herman <nherman@gmail.com>
2015-05-01 21:35:32 -0400

    updated bootstrap to include "real" data

--- poolscore/database/bootstrap.sql
+++ poolscore/database/bootstrap.sql
@@ -1,6 +1,6 @@
 insert into accounts
 	(username, email, firstname, lastname, language, active)
-	values ("admin", "", "admin", "user", "", 101);
+	values ("admin", "", "admin", "user", "", 1);
 insert into accounts
 	(username, email, firstname, lastname, language, active)
 	values ("player1", "", "player", "one", "", 1);
@@ -36,89 +36,167 @@ insert into division
 	(name, league_id, region_id, ruleset)
 	values ("East Village", 1, 1, "APA8BALL");
 
-insert into team
-	(name, location, league_num, account_id, league_id, division_id)
-	values ("Pool of Blood", "Josies's", 101, 1, 1, 1);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Team", 1, 1);
+
+--Team 1 - Mona's
 insert into team
 	(name, location, league_num, account_id, league_id, division_id)
 	values ("Mona's Athletic Club", "Mona's", 1105, 1, 1, 1);
 insert into permissions
 	(entity, row_id, account_id)
-	values ("Team", 2, 1);
-insert into team
-	(name, location, league_num, account_id, league_id, division_id)
-	values ("Player1 Team", "P1's", 1111, 2, 1, 1);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Team", 3, 2);
-insert into team
-	(name, location, league_num, account_id, league_id, division_id)
-	values ("Player2 Team", "P2's", 2222, 3, 1, 1);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Team", 4, 3);
-insert into team
-	(name, location, league_num, account_id, league_id, division_id)
-	values ("Parkside", "Parkside", 666, 1, 1, 1);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Team", 5, 1);
+	values ("Team", 1, 1);
 
 insert into player
 	(league_num, firstname, lastname, handicap)
-	values (1, "Dagwood", "Bumstead", 6);
+	values (06954, "Ray", "De Jesus", 5);
 insert into permissions
 	(entity, row_id, account_id)
 	values ("Player", 1, 1);
-insert into player
-	(league_num, firstname, lastname, handicap)
-	values (2, "Andy", "Cap", 4);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Player", 2, 1);
-insert into player
-	(league_num, firstname, lastname, handicap)
-	values (111, "Player", "One", 4);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Player", 3, 2);
-insert into player
-	(league_num, firstname, lastname, handicap)
-	values (222, "Player", "Two", 4);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Player", 4, 3);
-insert into player
-	(league_num, firstname, lastname, handicap)
-	values (666, "Guy", "Fiere", 2);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Player", 5, 1);
-insert into player
-	(league_num, firstname, lastname, handicap)
-	values (666, "Super", "Man", 2);
-insert into permissions
-	(entity, row_id, account_id)
-	values ("Player", 6, 1);
-
 insert into team_player
 	(team_id, player_id)
 	values (1,1);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (08754, "Jason", "Tenzer", 5);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 2, 1);
 insert into team_player
 	(team_id, player_id)
-	values (2,2);
+	values (1,2);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (11480, "Julie", "Sisson", 3);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 3, 1);
 insert into team_player
 	(team_id, player_id)
-	values (3,3);
+	values (1,3);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (12292, "Leslie", "Chung", 2);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 4, 1);
 insert into team_player
 	(team_id, player_id)
-	values (4,4);
+	values (1,4);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (07648, "Nick", "Herman", 6);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 5, 1);
 insert into team_player
 	(team_id, player_id)
-	values (5,5);
+	values (1,5);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (09454, "Genever", "McBain", 3);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 6, 1);
 insert into team_player
 	(team_id, player_id)
 	values (1,6);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (11102, "Haydee", "Jimenez", 4);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 7, 1);
+insert into team_player
+	(team_id, player_id)
+	values (1,7);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (13026, "Brian", "Daly", 3);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 8, 1);
+insert into team_player
+	(team_id, player_id)
+	values (1,8);
+
+
+--Team 2 - Double Down
+insert into team
+	(name, location, league_num, account_id, league_id, division_id)
+	values ("Double Down", "Double Down Saloon", 00806, 1, 1, 1);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Team", 2, 1);
+
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (10573, "Jona", "Herbert", 5);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 9, 1);
+insert into team_player
+	(team_id, player_id)
+	values (2,9);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (10717, "Patrick", "Dopke", 5);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 10, 1);
+insert into team_player
+	(team_id, player_id)
+	values (2,10);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (11757, "Eric", "Kalison", 3);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 11, 1);
+insert into team_player
+	(team_id, player_id)
+	values (2,11);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (12255, "Nick", "Mullendore", 4);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 12, 1);
+insert into team_player
+	(team_id, player_id)
+	values (2,12);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (12739, "John", "Bahal", 4);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 13, 1);
+insert into team_player
+	(team_id, player_id)
+	values (2,13);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (12155, "Mookie", "Rosa", 5);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 14, 1);
+insert into team_player
+	(team_id, player_id)
+	values (2,14);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (09907, "Ed", "Reinhardt", 3);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 15, 1);
+insert into team_player
+	(team_id, player_id)
+	values (2,15);
+insert into player
+	(league_num, firstname, lastname, handicap)
+	values (12376, "Andrew", "Whitney", 3);
+insert into permissions
+	(entity, row_id, account_id)
+	values ("Player", 16, 1);
+insert into team_player
+	(team_id, player_id)
+	values (2,16);
+
