a798209e057ffb6fd105113a09d92e09ce20a646
Nick Herman <nherman@gmail.com>
2015-10-03 22:13:04 -0400

    changed defualt date format:

--- poolscore/mod_common/jinja_globals.py
+++ poolscore/mod_common/jinja_globals.py
@@ -3,7 +3,7 @@ from poolscore.mod_common.utils import SecurityUtil
 def _is_admin():
     return SecurityUtil.is_admin()
 
-def datetime(value, format='%d-%m-%Y'):
+def datetime(value, format='%Y-%m-%d'):
     return value.strftime(format)    
 
 globals = {
