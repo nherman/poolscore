cc288b95ad0618d98d6a4b2d4b2c4c24a5f6e25d
nherman <nherman@gmail.com>
2016-03-01 11:39:50 -0500

    implemented tourney add page

--- poolscore/mod_play/controllers.py
+++ poolscore/mod_play/controllers.py
@@ -1,9 +1,10 @@
 import json
-from datetime import date
+from datetime import date, time, datetime
 
 from flask import Blueprint, request, render_template, \
-                  flash, g, session, redirect, url_for, send_from_directory
+                  flash, session, redirect, url_for
 
+from poolscore import db
 from poolscore.mod_common.utils import SecurityUtil
 from poolscore.mod_common.rulesets import Rulesets
 from poolscore.mod_team.models import Team
@@ -16,38 +17,43 @@ mod_play = Blueprint('play', __name__, url_prefix = '/play')
 @mod_play.route('/', methods = ['GET'])
 @SecurityUtil.requires_auth()
 def index():
-    tournies = Tourney.secure_all()
-    if (len(tournies) == 0):
+    tourneys = Tourney.secure_all()
+    if (len(tourneys) == 0):
         return redirect(url_for('play.new'))
-    if (len(tournies) == 1):
-        return redirect(url_for('play.play', id = tournies[0].id))
+    if (len(tourneys) == 1):
+        return redirect(url_for('play.play', id = tourneys[0].id))
 
-    return render_template('play/index.html', tournies = tournies)
+    return render_template('play/index.html', tourneys = tourneys)
 
 @mod_play.route('/new/', methods = ['GET', 'POST'])
 @SecurityUtil.requires_auth()
 def new():
-    print "NEW"
     form = NewTourneyForm(request.form)
     team_choices = [(-1,"Select a Team")] + [(t.id, "{} ({})".format(t.name, t.id)) for t in Team._query().all()]
     form.home_team_id.choices = team_choices
     form.away_team_id.choices = team_choices
-    form.date.data = date.today()
-    form.start_time.data = date.today() #add 19 hours?
 
     ruleset = scoring_method = "APA8BALL"
-    events = json.dumps(Rulesets[ruleset].tourney_events),
+    events = Rulesets[ruleset].tourney_events
 
     if form.validate_on_submit():
-        events['coin_toss'] = form.data.coin_toss
-        events['player_choice'] = form.data.player_choice
-        events['start_time'] = form.data.start_time
+        #convert date to time string for JSON serialization
+        if isinstance(form.start_time.data, datetime):
+            try:
+                start_time = form.start_time.data.time()
+                events['start_time'] = start_time.isoformat()
+            except TypeError():
+                pass
+
+        events['coin_toss'] = form.coin_toss.data
+        events['player_choice'] = form.player_choice.data
+        eventsJSON = json.dumps(events)
 
         tourney = Tourney(
             date = form.date.data,
             home_team_id = form.home_team_id.data,
             away_team_id = form.away_team_id.data,
-            events = events,
+            events = eventsJSON,
             scoring_method = scoring_method,
             ruleset = ruleset)
 
@@ -56,6 +62,11 @@ def new():
 
         flash('Tourney %s vs. %s has been added' % (tourney.home_team_id, tourney.away_team_id), 'success')
         return redirect(url_for('play.play', id = tourney.id))
+
+    if request.method == 'GET':
+        timestamp = datetime.now()
+        form.date.data = timestamp.today()
+        form.start_time.data = timestamp
 
     return render_template('play/new.html', form = form)
 
---
+++ poolscore/mod_play/forms.py
@@ -0,0 +1,13 @@
+from flask.ext.wtf import Form
+from wtforms import SelectField, DateField, DateTimeField
+from wtforms.validators import Required, NumberRange, Optional
+
+class NewTourneyForm(Form):
+    __abstract__ = True
+
+    home_team_id = SelectField('Home Team', [NumberRange(min=1,message = 'Select Home Team.')], coerce=int)
+    away_team_id = SelectField('Away Team', [NumberRange(min=1,message = 'Select Away Team.')], coerce=int)
+    date = DateField('Date', [Required(message = 'Enter tourney date')])
+    start_time = DateTimeField('Start Time', [Required(message = 'Enter start time')], format="%I:%M %p", default="07:00")
+    coin_toss = SelectField('Coin Toss', [Optional()], choices=[("","Select Home or Away"),('HOME','Home'),('AWAY','Away')])
+    player_choice = SelectField('First Player Choice', choices=[("","Select Home or Away"),('HOME','Home'),('AWAY','Away')])
