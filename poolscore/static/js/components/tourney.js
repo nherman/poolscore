df452c09681ae9beb4a810d638e9194dbe4dfee0
nicholas herman <nherman@gmail.com>
2017-12-04 18:07:13 -0500

    wired new match form (mostly)

--- poolscore/static/js/components/tourney.js
+++ poolscore/static/js/components/tourney.js
@@ -22,8 +22,31 @@ define('components/tourney', ['knockout','services/api',], function(ko, api) {
             throw "Missing rootVM reference";
         }
 
-        self.raw = {};
+        function getTourney() {
+            api.getTourney(options.tourney_id, function(json) {
+                console.log(json);
+                self.raw = json["tourney"];
 
+                self.date(self.raw.date);
+                self.winner_id(self.raw.winner_id);
+                self.home_score(self.raw.home_score || 0);
+                self.away_score(self.raw.away_score || 0);
+
+                self.home_team(self.raw.home_team);
+                self.away_team(self.raw.away_team);
+            });
+        }
+
+        function getMatches() {
+            api.getMatches(options.tourney_id, function(json) {
+                console.log(json);
+                self.matches(json["matches"]);
+            });
+        }
+
+        self.tourney_id = options.tourney_id;
+
+        self.raw = {};
         self.date = ko.observable();
         self.winner_id = ko.observable();
         self.home_score = ko.observable();
@@ -32,29 +55,64 @@ define('components/tourney', ['knockout','services/api',], function(ko, api) {
         self.home_team = ko.observable();
         self.away_team = ko.observable();
 
-        self.matches = ko.observableArray();;
+        self.matches = ko.observableArray();
+
+        self.lagger = ko.observable();
+        self.nonLagger = ko.observable();
+        self.newMatch = function() {
+            console.log(self.lagger());
+            console.log(self.nonLagger());
+
+            var lagger, nonLagger, data = {
+                    "match":{
+                        "events": {
+                            "lag": null
+                        }
+                    },
+                    "home_players": [],
+                    "away_players": []
+                };
+
+            function _p(o) {
+                var a = o.split('-');
+                return {
+                    "team": a[0],
+                    "id": a[1]
+                };
+            }
+
+            //validate select value format
+            if (!self.lagger().match(/.+-\d+/) || !self.nonLagger().match(/.+-\d+/)) return;
+
+            //parse team and player id from select values
+            lagger = _p(self.lagger());
+            nonLagger = _p(self.nonLagger());
+
+            //validate players from different teams
+            if (lagger.team == nonLagger.team) return;
+
+            //populate data
+            data[lagger.team + '_players'].push(lagger.id);
+            data[nonLagger.team + '_players'].push(nonLagger.id);
+            data.match.events.lag = lagger.team;
+
+            api.createMatch(options.tourney_id, data,
+                function(json) {
+                    //reset form
+                    self.lagger(-1);
+                    self.nonLagger(-1);
+
+                    getMatches();
+                }
+            );
+
+        }
 
         // self.events = {};
 
-        api.getTourney(options.tourney_id, function(json) {
-            console.log(json);
-            self.raw = json["tourney"];
-
-            self.date(self.raw.date);
-            self.winner_id(self.raw.winner_id);
-            self.home_score(self.raw.home_score || 0);
-            self.away_score(self.raw.away_score || 0);
-
-            self.home_team(self.raw.home_team);
-            self.away_team(self.raw.away_team);
-
-            self.matches(self.raw.matches);
-        });
-
-        api.getMatches(options.tourney_id, function(json) {
-            console.log(json);
-            self.matches(json["matches"]);
-        });
+        //initialize view model
+        getTourney();
+        getMatches();
 
     }
 
